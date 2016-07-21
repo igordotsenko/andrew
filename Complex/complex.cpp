6414324045d968296e616b799f4aac743c4f9aa8
gymfox <volpeandre193@gmail.com>
2016-07-21 22:20:28 +0300

    Complex task

---
+++ Complex/complex.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <cmath>
+
+#include "Complex.h"
+
+using namespace std;
+
+Complex::Complex(double real, double imaginary) {
+    this->real = real;
+    this->imaginary = imaginary;
+}
+
+Complex::~Complex() {}
+
+double Complex::getReal() const {
+    return real;
+}
+
+double Complex::getImaginary() const {
+    return imaginary;
+}
+
+bool Complex::operator==(const Complex& other) const {
+    return real == other.real && imaginary == other.imaginary;
+}
+
+bool Complex::operator!=(const Complex& other) const {
+    return *this != other;
+}
+
+void Complex::operator+=(const Complex& other) {
+    Complex result = *this;
+    
+    result.real += other.real;
+    result.imaginary += other.real;
+}
+
+void Complex::operator-=(const Complex& other) {
+    Complex result = *this;
+
+    result.real -= other.real;
+    result.imaginary -= other.real;
+}
+
+Complex Complex::operator+(const Complex& other) const {
+    Complex result = *this;
+
+    result.real += other.real;
+    result.imaginary += other.imaginary;
+    
+    return result;
+}
+
+
+Complex Complex::operator-(const Complex& other) const {
+    Complex result = *this;
+
+    result.real -= other.real;
+    result.imaginary -= other.imaginary;
+
+    return result;
+}
+
+Complex Complex::operator*(const Complex& other) const {
+    Complex result = *this;
+
+    result.real *= other.real;
+    result.imaginary *= other.imaginary;
+
+    return result;
+    }
+
+std::ostream& operator<<(std::ostream& out, const Complex& complex) {
+    if ( complex.getImaginary() < 0 ) {
+        return out << complex.getReal() << complex.getImaginary();
+    }
+
+    return out << complex.getReal() << '+' << complex.getImaginary();
+}
