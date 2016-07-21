8b6fa833eec939987cb5dd4c419114359da0eec5
gymfox <volpeandre193@gmail.com>
2016-07-21 14:00:49 +0300

    Car task

---
+++ Car/point.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cmath>
+
+#include "Point.h"
+
+using namespace std;
+
+Point::Point(double x, double y) {
+    this->x = x;
+    this->y = y;
+} 
+
+Point::~Point() {}
+
+double Point::getX() const {
+    return x;
+}
+
+double Point::getY() const {
+    return y;
+}
+
+double Point::distance(const Point& other) const {
+    return hypot(other.x-x, other.y-y);
+}
+
+bool Point::operator==(const Point& other) const {
+    return x == other.x && y == other.y;
+}
+
+bool Point::operator!=(const Point& other) const {
+    return !(*this == other);
+}
+
+ostream& operator<<(ostream& out, const Point& other) {
+    return out << '(' << other.getX() << ", " << other.getY() << ')';
+}
