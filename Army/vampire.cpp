2956ddbaca0f49305103ca301b8a39920f01dbfc
gymfox <solod.andrew93@gmail.com>
2017-03-25 21:19:26 +0200

    corrected vampire class

--- Army/vampire.cpp
+++ Army/vampire.cpp
@@ -8,10 +8,11 @@ Vampire::~Vampire() {}
 
 void Vampire::attack(Unit* victim) {
     ensureIsAlive();
+
     victim->takeDamage(getDamage());
 
     vampirism(victim);
-    cout << "\t" << getName() << " taked " << getDamage() << " damage and recovered " << victim->getDamage()*0.4 << " HP" << endl;
+    cout << "\t" << getName() << " hit for " << getDamage() << " damage and recovered " << victim->getCurrentHP() / 10 << " HP\n";
 
     victim->counterAttack(this);
 }
@@ -19,23 +20,22 @@ void Vampire::attack(Unit* victim) {
 void Vampire::counterAttack(Unit* victim) {
     ensureIsAlive();
 
-    victim->takeDamage(getDamage()/2);
+    victim->takeDamage(getDamage() / 2);
     vampirism(victim);
 
-    cout << "\t" << getName() << " counterattacking with " << getDamage()/2 << " damage and recovered " << victim->getDamage()*0.4 << " HP\n" << endl;
+    cout << "\t" << getName() << " is counter attacking for " << getDamage()/2 << " damage and recovering " << getCurrentHP() / 10 << " HP\n" << endl;
 }
 
 void Vampire::vampirism(Unit* victim) {
     ensureIsAlive();
 
-    int recoverHealthPoint = victim->getDamage() * 0.4;
+    int recoverHP = victim->getCurrentHP() / 10;
 
-    victim->takeDamage(victim->getDamage()*0.4);
-    if ( getHealthtPointsLimit() - getCurrentHealthPoint() < recoverHealthPoint ) {
-        currentHealthPoint = getHealthtPointsLimit();
+    if ( getHPLimit() - getCurrentHP() < recoverHP ) {
+        setCurrentHP(getHPLimit());
 
         return;
     }
 
-    currentHealthPoint += recoverHealthPoint;
+    setCurrentHP(getCurrentHP() + recoverHP);
 }
