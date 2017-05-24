60e272cc5ecfd46e8bf98f913212842c2425f234
gymfox <solod.andrew93@gmail.com>
2017-05-24 23:13:46 +0300

    deleted old files

--- Army/humanstate.cpp
+++
@@ -1,9 +0,0 @@
-#include "humanstate.h"
-
-HumanState::HumanState(Unit* currentStateUnit) : State(currentStateUnit) {
-    this->healthPointsLimit = currentStateUnit->getHPLimit();
-    this->currentHP = currentStateUnit->getHPLimit();
-    this->damage = currentStateUnit->getDamage();
-}
-
-HumanState::~HumanState() {}
--- Army/soldier.cpp
+++
@@ -1,7 +0,0 @@
-#include "soldier.h"
-
-using namespace std;
-
-Soldier::Soldier(const string& name, int healthPoint, int damage) : Unit(name, healthPoint, damage) {}
-
-Soldier::~Soldier() {}
