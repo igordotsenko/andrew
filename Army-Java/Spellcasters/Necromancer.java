c389dbb64a7686df692af502b360df6754bb040f
gymfox <solod.andrew93@gmail.com>
2017-07-16 17:36:40 +0300

    Necromancers cannot added berserk to observable lists

--- Army-Java/Spellcasters/Necromancer.java
+++ Army-Java/Spellcasters/Necromancer.java
@@ -1,8 +1,12 @@
 package com.gymfox.Army.Spellcasters;
 
 import com.gymfox.Army.Ability.NecroAbility;
-import com.gymfox.Army.Ability.WizardAbility;
+import com.gymfox.Army.Exception.IsSelfAttackException;
+import com.gymfox.Army.Exception.ManaIsOverException;
+import com.gymfox.Army.Exception.MasterAttackedException;
+import com.gymfox.Army.Exception.UnitIsDeadException;
 import com.gymfox.Army.State.HumanState;
+import com.gymfox.Army.Units.Unit;
 
 public class Necromancer extends Spellcaster {
     public Necromancer(String name, int healthPointLimit, int damage, int manaPointLimits) {
@@ -13,5 +17,20 @@ public class Necromancer extends Spellcaster {
         setNextState(new HumanState(this));
         setCurrentSpell("Fireball");
         setDead();
+    }
+
+    @Override
+    public void castSpell(Unit victim) throws IsSelfAttackException, UnitIsDeadException, ManaIsOverException {
+        if ( victim.getUnitType() == UnitType.BERSERK ) {
+            return;
+        }
+        addObservable(victim);
+        super.castSpell(victim);
+    }
+
+    @Override
+    public void attack(Unit victim) throws UnitIsDeadException, IsSelfAttackException, MasterAttackedException {
+        addObservable(victim);
+        super.attack(victim);
     }
 }
