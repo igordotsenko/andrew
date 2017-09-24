052f7c9a41e388de2807569d5af99d3c4ab919cf
Andrew Solod <gymfox@users.noreply.github.com>
2017-09-24 16:15:03 +0300

    removed Summon

--- Army-Java/Spells/Summon.java
+++
@@ -1,9 +0,0 @@
-package com.gymfox.Army.Spells;
-
-public class Summon extends Spell {
-    public Summon() {
-        this.spellsName = "Summon";
-        this.manaConsumption = 12;
-        this.hitPoints = 0;
-    }
-}
