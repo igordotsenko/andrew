0e46d31f5e3879af77b43d159d38aaa53548b5ac
gymfox <solod.andrew93@gmail.com>
2017-10-23 20:56:29 +0300

    Implemented IPv4address class

---
+++ Communication/src/com/gymfox/communication/ipv4address/IPv4Address.java
@@ -0,0 +1,92 @@
+
+package com.gymfox.communication.ipv4address;
+
+public class IPv4Address {
+    private final long LONG_MAX_IP = 4294967295l;
+    private final int MAX_OCTETS_VALUE = 255;
+    private final int MIN_OCTETS_VALUE = 0;
+    private final int OCTETS = 4;
+
+    private String ipAsString;
+    private long ipAsLong;
+    private boolean isString = false;
+
+    public static class InvalidOctetsCountException extends Exception{};
+    public static class InvalidValueInOctetsException extends Exception{};
+
+    public IPv4Address(String address) throws IllegalArgumentException,
+            InvalidValueInOctetsException, InvalidOctetsCountException {
+        validate(address);
+        this.ipAsString = address;
+        this.isString = true;
+    }
+
+    public IPv4Address(long address) throws IllegalArgumentException {
+        validate(address);
+        this.ipAsLong = address;
+    }
+
+    private void validate(String address) throws IllegalArgumentException,
+            InvalidOctetsCountException, InvalidValueInOctetsException {
+        String[] octets = address.split("\\.");
+
+        if ( octets.length != OCTETS) {
+            throw new InvalidOctetsCountException();
+        }
+
+        for ( String eachOctets : octets ) {
+            int octetsNumbers = Integer.parseInt(eachOctets);
+
+            if ( octetsNumbers < MIN_OCTETS_VALUE || octetsNumbers > MAX_OCTETS_VALUE ) {
+                throw new InvalidValueInOctetsException();
+            }
+        }
+    }
+
+    private void validate(long address) {
+        if ( address > LONG_MAX_IP || address < 0 ) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public boolean lessThan(IPv4Address address) {
+        return this.toLong() < address.toLong();
+    }
+
+    public boolean greaterThan(IPv4Address address) {
+        return this.toLong() > address.toLong();
+    }
+
+    public boolean equals(IPv4Address address) {
+        return this.toLong() == address.toLong();
+    }
+
+    public String toString() {
+        if ( !isString ) {
+            StringBuffer out = new StringBuffer();
+
+            for ( int i = 3; i >= 0; i-- ) {
+                int shift = i * 8;
+
+                out.append((ipAsLong >> shift) & 255);
+                if ( i > 0 ) {
+                    out.append(".");
+                }
+            }
+            return out.toString();
+        }
+        return ipAsString;
+    }
+
+    public long toLong() {
+        if (isString) {
+            String[] octets = ipAsString.split("\\.");
+
+            for ( String s : octets ) {
+                ipAsLong = ipAsLong * 256 + Integer.parseInt(s);
+            }
+            return ipAsLong;
+        }
+        return ipAsLong;
+    }
+}
---
+++ Communication/test/com/gymfox/communication/ipv4address/IPv4AddressTest.java
@@ -0,0 +1,87 @@
+package com.gymfox.communication.ipv4address;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class IPv4AddressTest {
+    @Test
+    public void equalsIpAsStringTest() throws IPv4Address.InvalidOctetsCountException,
+            IPv4Address.InvalidValueInOctetsException {
+        Assert.assertEquals("255.255.255.255", new IPv4Address("255.255.255.255").toString());
+        Assert.assertEquals("127.12.45.22", new IPv4Address("127.12.45.22").toString());
+        Assert.assertEquals("189.11.23.211", new IPv4Address("189.11.23.211").toString());
+    }
+
+    @Test
+    public void equalsIpAsLongWithStringTest() throws IPv4Address.InvalidOctetsCountException,
+            IPv4Address.InvalidValueInOctetsException {
+        Assert.assertEquals(2131504406l, new IPv4Address("127.12.45.22").toLong());
+        Assert.assertEquals(3171620819l, new IPv4Address("189.11.23.211").toLong());
+        Assert.assertEquals(4294967295l, new IPv4Address("255.255.255.255").toLong());
+    }
+
+    @Test
+    public void equalsIpAsLongTest() {
+        Assert.assertEquals(2131504406l, new IPv4Address(2131504406l).toLong());
+        Assert.assertEquals(3171620819l, new IPv4Address(3171620819l).toLong());
+        Assert.assertEquals(4294967295l, new IPv4Address(4294967295l).toLong());
+    }
+
+    @Test
+    public void equalsStringWithLongTest() throws IPv4Address.InvalidOctetsCountException,
+            IPv4Address.InvalidValueInOctetsException {
+        Assert.assertEquals(true, (new IPv4Address("127.12.45.22")).equals(new IPv4Address(2131504406l)));
+        Assert.assertEquals(true, (new IPv4Address("189.11.23.211")).equals(new IPv4Address(3171620819l)));
+    }
+
+    @Test
+    public void ipStringGraterThanTest() throws IPv4Address.InvalidOctetsCountException,
+            IPv4Address.InvalidValueInOctetsException {
+        IPv4Address ip1 = new IPv4Address("189.11.23.211");
+        IPv4Address ip2 = new IPv4Address("127.12.45.22");
+
+        Assert.assertEquals(true, ip1.greaterThan(ip2));
+    }
+
+    @Test
+    public void ipLongGraterThanTest() {
+        IPv4Address ip1 = new IPv4Address(3171620819l);
+        IPv4Address ip2 = new IPv4Address(2131504406l);
+
+        Assert.assertEquals(true, ip1.greaterThan(ip2));
+    }
+
+    @Test
+    public void ipStringLessThanTest() {
+        IPv4Address ip1 = new IPv4Address(2131504406l);
+        IPv4Address ip2 = new IPv4Address(3171620819l);
+
+        Assert.assertEquals(true, ip1.lessThan(ip2));
+    }
+
+    @Test ( expected = IllegalArgumentException.class )
+    public void validateLongTest() {
+        IPv4Address ip = new IPv4Address(4294967296l);
+    }
+
+    @Test ( expected = IPv4Address.InvalidOctetsCountException.class )
+    public void validateOctetsCountTest() throws IPv4Address.InvalidOctetsCountException,
+            IPv4Address.InvalidValueInOctetsException {
+        IPv4Address ip = new IPv4Address("255.255.255.255.255");
+    }
+
+    @Test ( expected = IPv4Address.InvalidOctetsCountException.class )
+    public void notEnoughOctetsTest() throws IPv4Address.InvalidOctetsCountException,
+            IPv4Address.InvalidValueInOctetsException {
+        IPv4Address ip = new IPv4Address("255.255");
+    }
+
+    @Test ( expected = IPv4Address.InvalidValueInOctetsException.class )
+    public void validateNumbersInOctetsTest() throws IPv4Address.InvalidOctetsCountException,
+            IPv4Address.InvalidValueInOctetsException {
+        IPv4Address ip = new IPv4Address("999.999.999.999");
+    }
+}
+
+
+
