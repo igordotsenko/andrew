a543473f0c04c8fd5ccaf907e492937533efb134
gymfox <solod.andrew93@gmail.com>
2018-08-31 14:58:20 +0300

    added IPv4Address

---
+++ Java/src/com/gymfox/communication/IPv4Address.java
@@ -0,0 +1,118 @@
+package com.gymfox.communication;
+
+public class IPv4Address {
+    protected static final long LONG_MAX_IP = 4294967295l;
+    protected static final int MAX_OCTETS_VALUE = 255;
+    private static final int MIN_OCTETS_VALUE = 0;
+    private static final int OCTETS_COUNT = 4;
+
+    private final String ipAsString;
+    private final long ipAsLong;
+
+    public static class InvalidValueInOctetsException extends IllegalArgumentException {
+        public InvalidValueInOctetsException(String errorMessage) {
+            super(errorMessage);
+        }
+    }
+
+    public static class InvalidOctetsCountException extends  IllegalArgumentException {
+        public InvalidOctetsCountException(String errorMessage) {
+            super(errorMessage);
+        }
+    }
+
+    public IPv4Address(String address) throws IllegalArgumentException {
+        this.ipAsLong = ipToLong(address);
+        this.ipAsString = address;
+    }
+
+    public IPv4Address(long address) throws IllegalArgumentException {
+        this.ipAsString = ipToString(address);
+        this.ipAsLong = address;
+    }
+
+    private long ipToLong(String address) {
+        String[] octets = address.split("\\.");
+
+        validateOctetsCount(octets);
+
+        long newIpAsLong = 0;
+
+        for ( String s : octets ) {
+            long octetValue = Long.parseLong(s);
+
+            validateOctetValue(octetValue);
+            newIpAsLong = newIpAsLong * 256 + octetValue;
+        }
+
+        return newIpAsLong;
+    }
+
+    private String ipToString(long ipAsLong) {
+        validateLongValue(ipAsLong);
+        long[] octets = new long[4];
+
+        octets[0] = ( ipAsLong & 0xFF000000L ) >> 24;
+        octets[1] = ( ipAsLong & 0x00FF0000L ) >> 16;
+        octets[2] = ( ipAsLong & 0x0000FF00L ) >> 8;
+        octets[3] = ( ipAsLong & 0x000000FFL );
+
+        return String.format("%d.%d.%d.%d", octets[0], octets[1], octets[2], octets[3]);
+    }
+
+    private void validateOctetsCount(String[] splitOctets) {
+        if ( splitOctets.length != OCTETS_COUNT ) {
+            throw new InvalidOctetsCountException(String.format("%d octets are expected. Got %d for IP %s instead.",
+                    OCTETS_COUNT, splitOctets.length, buildStringFromOctets(splitOctets)));
+        }
+    }
+
+    private String buildStringFromOctets(String[] octets) {
+        StringBuffer wrongIP = new StringBuffer();
+
+        for ( String s : octets ) {
+            wrongIP.append(s +".");
+        }
+        wrongIP.delete(wrongIP.length()-1, wrongIP.length());
+
+        return wrongIP.toString();
+    }
+
+    private void validateOctetValue(long octetsNumbers) {
+        if ( octetsNumbers < MIN_OCTETS_VALUE || octetsNumbers > MAX_OCTETS_VALUE ) {
+            throw new InvalidValueInOctetsException(String.format("%d octet is incorrect. Expected less than %d.",
+                    octetsNumbers, MAX_OCTETS_VALUE));
+        }
+    }
+
+    private void validateLongValue(long address) {
+        if ( address > LONG_MAX_IP || address < 0 ) {
+            throw new InvalidValueInOctetsException(String.format("%d value is wrong.", address));
+        }
+    }
+
+    @Override
+    public String toString() {
+        return getIpString() + " = " + getIpLong();
+    }
+
+    public boolean lessThan(IPv4Address address) {
+        return this.getIpLong() <= address.getIpLong();
+    }
+
+    public boolean greaterThan(IPv4Address address) {
+        return this.getIpLong() >= address.getIpLong();
+    }
+
+    public boolean equals(IPv4Address address) {
+        return this.getIpLong() == address.getIpLong();
+    }
+
+    public String getIpString() {
+        return ipAsString;
+    }
+
+    public long getIpLong() {
+        return ipAsLong;
+    }
+}
