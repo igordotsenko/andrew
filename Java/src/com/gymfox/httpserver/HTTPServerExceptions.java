4ce7d1c68a5ff531f99ded668ae8264e2f2d7953
gymfox <solod.andrew93@gmail.com>
2018-09-19 19:36:09 +0300

    Change access.

---
+++ Java/src/com/gymfox/httpserver/HTTPServerExceptions.java
@@ -0,0 +1,43 @@
+package com.gymfox.httpserver;
+
+import java.net.ProtocolException;
+import java.net.SocketException;
+import java.nio.file.NoSuchFileException;
+
+class HTTPServerExceptions {
+    static class FileIsEmptyException extends NoSuchFileException {
+        FileIsEmptyException(String errorMessage) {
+            super(errorMessage);
+        }
+    }
+
+    static class InvalidPortException extends SocketException {
+        InvalidPortException(String errorMessage) {
+            super(errorMessage);
+        }
+    }
+
+    static class NotAllowedMethodException extends ProtocolException {
+        NotAllowedMethodException(String errorMessage) {
+            super(errorMessage);
+        }
+    }
+
+    static class InvalidHttpVersionException extends ProtocolException {
+        InvalidHttpVersionException(String errorMessage) {
+            super(errorMessage);
+        }
+    }
+
+    static class HttpServerIsRunningException extends SocketException {
+        HttpServerIsRunningException(String errorMessage) {
+            super(errorMessage);
+        }
+    }
+
+    static class InvalidPartsHTTPVersionException extends ProtocolException {
+        InvalidPartsHTTPVersionException(String errorMessage) {
+            super(errorMessage);
+        }
+    }
+}
